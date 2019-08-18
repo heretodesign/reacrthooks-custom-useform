fa28cc1f8ab9162a67ec21d5f531699cd1c09a25
heretodesign <senwelo.works@gmail.com>
2019-08-18 20:37:18 +0800

    refactored the code and added the handlesubmit method to useform page

--- src/formContainer/useForm.js
+++ src/formContainer/useForm.js
@@ -2,22 +2,22 @@ import React, {useState} from 'react';
 import { Button, Columns, Navbar, Header} from "react-bulma-components/full"
 import 'react-bulma-components/dist/react-bulma-components.min.css'
 
-const useForm = () => {
-
-  // const [title, setTitle] = useState("");
-  // const [date, setDate] = useState("");
-  // const [content, setContent] = useState("");
-  // const [comment, setComment] = useState("");
+const useForm = submitCallback => {
 
 
   const [state, setState] = useState({});
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    submitCallback();
+  }
 
   const handleChange = e => {
     e.persist();
     setState(state => ({ ...state, [e.target.name]: e.target.value }));
   }
 
-  return [state, handleChange];
+  return [state, handleChange, handleSubmit];
 }
 
 export default useForm;
